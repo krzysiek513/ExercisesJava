c30f078e9db41f8fbd845a17f0a7c6e0c69821aa
krzysiek513 <krzysiek3155@gmail.com>
2020-09-24 13:05:20 +0200

    pierwsze cwiczenie

---
+++ src/Ch2_Intro/SamplePayroll.java
@@ -0,0 +1,10 @@
+package Ch2_Intro;
+
+public class SamplePayroll {
+    public static void main(String[] args) {
+        System.out.print("Enter an integer: ");
+        int a, b =2, c=3;
+        a=b*c;
+        // Program performs a simple payroll calculation.
+    }
+}
